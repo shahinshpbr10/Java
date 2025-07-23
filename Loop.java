0155c04185c585acdadba4def3bb45b81b956c73
SHAHINSH <123875287+shahinshpbr10@users.noreply.github.com>
2025-07-23 09:11:49 +0000

    java loops added for babies

---
+++ Loop.java
@@ -0,0 +1,31 @@
+public class Loop{
+  public static void main (String[]  args){
+    //For Loop
+    
+    for(int i = 0; i<10; i++){
+       System.out.println("Ittration number : "+i);
+    }
+
+     //while loop
+
+    int count = 2;
+    while(count<10){
+      System.out.println("Ittration numbber for while loop : "+count);
+      count++;
+    }
+
+    //do while loop
+
+    int count1= 10;
+    do{
+      System.out.println("Ittration number for do while loop : "+count1);
+      count1++;
+    }while(count1<10);
+      
+   
+  }
+
+
+  
+}
+
